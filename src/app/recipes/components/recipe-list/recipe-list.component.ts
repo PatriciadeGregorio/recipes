0df6d391614a71374e0595de82a887daa59b7c73
PatriciadeGregorio <patricia.degre>
2019-05-07 20:11:49 +0200

    Refactor components

--- src/app/recipes/components/recipe-list/recipe-list.component.ts
+++ src/app/recipes/components/recipe-list/recipe-list.component.ts
@@ -1,49 +1,24 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { RecipeService } from '../../services/recipe.service';
 import { Recipe } from 'src/app/core/entities/recipe';
-import { RecipeResponse } from 'src/app/core/entities/recipeResponse';
-import { ActivatedRoute } from '@angular/router';
-import * as _ from 'lodash';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.scss']
 })
-export class RecipeListComponent implements OnInit {
-  recipes: Recipe[];
-  page = 1;
-  @Input() search: string;
+export class RecipeListComponent {
+  numberPage = 1;
+  @Input() recipes: Recipe[];
+  @Output()pagination = new EventEmitter<number>();
 
-  constructor(public recipeService: RecipeService, private route: ActivatedRoute) {
-  }
 
-  ngOnInit() {
-    this.recipes = this.route.snapshot.data['recipes'].results;
-  }
-
-  ngOnChanges() {
-    if (!_.isUndefined(this.search)) {
-      this.recipeService
-        .getRecipes({ recipe: this.search })
-        .subscribe((resp: RecipeResponse) => {
-          this.initializeData();
-          this.recipes = resp.results;
-        });
-    }
+  constructor(public recipeService: RecipeService) {
   }
 
   onScroll() {
-    this.page++;
-    this.recipeService
-      .getRecipes({ recipe: this.search, page: this.page })
-      .subscribe((resp: RecipeResponse) => {
-        this.recipes = this.recipes.concat(resp.results);
-      });
-  }
+    this.numberPage++;
+    this.pagination.emit(this.numberPage);
 
-  initializeData() {
-    this.recipes = [];
-    this.page = 1;
   }
 }
--- src/app/recipes/recipes/recipes.component.ts
+++ src/app/recipes/recipes/recipes.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { RecipeService } from '../services/recipe.service';
+import { Recipe } from 'src/app/core/entities/recipe';
+import { RecipeResponse } from 'src/app/core/entities/recipeResponse';
+import { ActivatedRoute } from '@angular/router';
+import * as _ from 'lodash';
 
 @Component({
   selector: 'app-recipes',
@@ -7,18 +12,43 @@ import { Component } from '@angular/core';
 })
 export class RecipesComponent {
   searchRecipe = '';
+  page = 1;
+  recipes: Recipe[];
 
+  constructor(
+    public recipeService: RecipeService,
+    private route: ActivatedRoute
+  ) {}
 
-  constructor() {
-
+  ngOnInit() {
+    this.recipes = this.route.snapshot.data['recipes'].results;
   }
 
   onSearch(searchTerm) {
-    if (typeof(searchTerm) === 'string') {
+    if (typeof searchTerm === 'string') {
       this.searchRecipe = searchTerm;
+      if (!_.isUndefined(this.searchRecipe)) {
+        this.recipeService
+          .getRecipes({ recipe: this.searchRecipe })
+          .subscribe((resp: RecipeResponse) => {
+            this.initializeData();
+            this.recipes = resp.results;
+          });
+      }
     }
   }
 
+  onPagination(page) {
+    this.page = page;
+    this.recipeService
+      .getRecipes({ recipe: this.searchRecipe, page: this.page })
+      .subscribe((resp: RecipeResponse) => {
+        this.recipes = this.recipes.concat(resp.results);
+      });
+  }
 
-
+  initializeData() {
+    this.recipes = [];
+    this.page = 1;
+  }
 }
